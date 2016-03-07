92732e87f1e426b948b6a68b71590ddb8048a74d
Topbit Du <topbit.du@gmail.com>
2016-03-07 09:11:31 +0800

    1, Improved the Beacon Page Relation wrapper class for coding style.

--- lib/wechat/shake_around/beacon_page_relation.rb
+++ lib/wechat/shake_around/beacon_page_relation.rb
@@ -4,7 +4,36 @@ class Wechat::ShakeAround::BeaconPageRelation
 
   extend ::Wechat::ShakeAround::Common
 
-  # 配置设备与页面的关联关系
+  # 删除设备与页面的关联关系
+  # http://mp.weixin.qq.com/wiki/12/c8120214ec0ba08af5dfcc0da1a11400.html
+  #
+  # Return hash format if success:
+  # {
+  #   data:    {},
+  #   errcode: 0,
+  #   errmsg:  'success.'
+  # }
+  #
+  # device_id is an integer or a hash like { uuid: <UUID>, major: <MAJOR>, minor: <MINOR> }.
+  # page_id is an integer or an integer array.
+  # bind 关联操作标志位，0为解除关联关系，1为建立关联关系
+  # append 新增操作标志位，0为覆盖，1为新增
+  def self.destroy(access_token, device_id, page_id)
+
+    device_identifier = normalize_device_id device_id
+    page_ids          = normalize_page_ids  page_id
+
+    message = ::JSONClient.new.post "https://api.weixin.qq.com/shakearound/device/bindpage?access_token=#{access_token}",
+      {
+        device_identifier: device_identifier,
+        page_ids:          page_ids, 
+        bind:              0,
+        append:            0
+      }
+    message.body
+  end
+
+  # 创建设备与页面的关联关系
   # http://mp.weixin.qq.com/wiki/12/c8120214ec0ba08af5dfcc0da1a11400.html
   #
   # Return hash format if success:
@@ -33,37 +62,4 @@ class Wechat::ShakeAround::BeaconPageRelation
     message.body
   end
 
-  # 配置设备与页面的关联关系
-  # http://mp.weixin.qq.com/wiki/12/c8120214ec0ba08af5dfcc0da1a11400.html
-  #
-  # Return hash format if success:
-  # {
-  #   data:    {},
-  #   errcode: 0,
-  #   errmsg:  'success.'
-  # }
-  #
-  # device_id is an integer or a hash like { uuid: <UUID>, major: <MAJOR>, minor: <MINOR> }.
-  # page_id is an integer or an integer array.
-  # bind 关联操作标志位，0为解除关联关系，1为建立关联关系
-  # append 新增操作标志位，0为覆盖，1为新增
-  def self.destroy(access_token, device_id, page_id)
-
-    device_identifier = normalize_device_id device_id
-    page_ids          = normalize_page_ids  page_id
-
-    message = ::JSONClient.new.post "https://api.weixin.qq.com/shakearound/device/bindpage?access_token=#{access_token}",
-      {
-        device_identifier: device_identifier,
-        page_ids:          page_ids, 
-        bind:              0,
-        append:            0
-      }
-    message.body
-  end
-=begin
-  def self.normalize_page_id(page_id)
-    page_id.is_a?(Array) ? page_id.map { |i| i.to_i } : [ page_id.to_i ]
-  end
-=end
 end
