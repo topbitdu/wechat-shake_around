771fc9a5ea3f39094ffdcdf5a5d4c1d9d137edd1
Topbit Du <topbit.du@gmail.com>
2016-10-13 09:40:04 +0800

    1, Improve the Device Report wrapper class for the argument validation.

--- lib/wechat/shake_around/device_report.rb
+++ lib/wechat/shake_around/device_report.rb
@@ -39,6 +39,7 @@ class Wechat::ShakeAround::DeviceReport
   def self.index(access_token, date, page_index = 1)
 
     assert_present! :access_token, access_token
+    assert_present! :date, date
 
     message = ::JSONClient.new.post "https://api.weixin.qq.com/shakearound/statistics/devicelist?access_token=#{access_token}",
       {
