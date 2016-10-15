323aeb4a9fe413905f684e25ecfd1ff7ddd2588b
Topbit Du <topbit.du@gmail.com>
2016-10-15 20:38:31 +0800

    1, Improve the Page Daily Report wrapper class for the argument validation.

--- lib/wechat/shake_around/page_daily_report.rb
+++ lib/wechat/shake_around/page_daily_report.rb
@@ -31,6 +31,7 @@ class Wechat::ShakeAround::PageDailyReport
 
     assert_present! :access_token, access_token
     assert_present! :page_id, page_id
+    assert_present! :date_range, date_range
 
     message = ::JSONClient.new.post "https://api.weixin.qq.com/shakearound/statistics/page?access_token=#{access_token}",
       {
