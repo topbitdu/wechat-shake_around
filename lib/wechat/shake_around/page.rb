748b3d7167424af21c70e20a6ecbdbcb50e03f08
Topbit Du <topbit.du@gmail.com>
2016-11-06 09:36:04 +0800

    1, Improve the Page wrapper class for the coding style.

--- lib/wechat/shake_around/page.rb
+++ lib/wechat/shake_around/page.rb
@@ -32,7 +32,7 @@ class Wechat::ShakeAround::Page
   def self.index(access_token, offset, limit)
 
     assert_present! :access_token, access_token
-    assert_present! :offset, offset
+    assert_present! :offset,       offset
     assert_present! :limit, limit
 
     message = ::JSONClient.new.post "https://api.weixin.qq.com/shakearound/page/search?access_token=#{access_token}",
