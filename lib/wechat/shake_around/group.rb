1943a9e09ba0d57af3bc91f150ff673939dbcb54
Topbit Du <topbit.du@gmail.com>
2016-11-27 10:05:15 +0800

    1, Improve the Group wrapper class to remove the un-used source codes.

--- lib/wechat/shake_around/group.rb
+++ lib/wechat/shake_around/group.rb
@@ -149,7 +149,6 @@ class Wechat::ShakeAround::Group
 
     assert_present! :access_token, access_token
     assert_present! :name,         name
-    #raise ArgumentError.new('The access_token argument is required.') if access_token.blank?
 
     message = ::JSONClient.new.post "https://api.weixin.qq.com/shakearound/device/group/add?access_token=#{access_token}", { group_name: name }
     message.body
