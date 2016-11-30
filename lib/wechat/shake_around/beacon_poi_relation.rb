816d66b70f45ac1a98660b7516d7328f1a67251b
Topbit Du <topbit.du@gmail.com>
2016-11-30 09:25:04 +0800

    1, Improve the Beacon PoI Relation wrapper class for the HTTP request.

--- lib/wechat/shake_around/beacon_poi_relation.rb
+++ lib/wechat/shake_around/beacon_poi_relation.rb
@@ -23,7 +23,7 @@ class Wechat::ShakeAround::BeaconPoiRelation
     assert_present! :poi_id,       poi_id
 
     device_identifier = self.normalize_device_id device_id
-    message = ::JSONClient.new.post "https://api.weixin.qq.com/shakearound/device/bindlocation?access_token=#{access_token}",
+    message = post_json "https://api.weixin.qq.com/shakearound/device/bindlocation?access_token=#{access_token}", body:
       {
         device_identifier: device_identifier,
         poi_id:            poi_id.to_i
