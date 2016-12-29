922b6ef6977bb58834aa1cd37c938a4811a18bd0
Topbit Du <topbit.du@gmail.com>
2016-12-29 12:09:40 +0800

    1, Improve the Beacon wrapper class for the HTTP request.

--- lib/wechat/shake_around/beacon.rb
+++ lib/wechat/shake_around/beacon.rb
@@ -106,7 +106,7 @@ class Wechat::ShakeAround::Beacon
     assert_present! :comment,      comment
 
     device_identifier = self.normalize_device_id device_id
-    message = ::JSONClient.new.post "https://api.weixin.qq.com/shakearound/device/update?access_token=#{access_token}",
+    message = post_json "https://api.weixin.qq.com/shakearound/device/update?access_token=#{access_token}",
       {
         device_identifier: device_identifier,
         comment:           comment
