f9a95e07bf9ace3928b148c59013976277d19882
Topbit Du <topbit.du@gmail.com>
2017-03-20 16:32:27 +0800

    1, Improve the Beacon Page Relation wrapper class for the HTTP request.

--- lib/wechat/shake_around/beacon_page_relation.rb
+++ lib/wechat/shake_around/beacon_page_relation.rb
@@ -30,14 +30,13 @@ class Wechat::ShakeAround::BeaconPageRelation
     device_identifier = normalize_device_id device_id
     page_ids          = normalize_page_ids  page_id
 
-    message = post_json "https://api.weixin.qq.com/shakearound/device/bindpage?access_token=#{access_token}", body:
+    post_json "https://api.weixin.qq.com/shakearound/device/bindpage?access_token=#{access_token}", body:
       {
         device_identifier: device_identifier,
         page_ids:          page_ids,
         bind:              0,
         append:            0
       }
-    message.body
   end
 
   ##
